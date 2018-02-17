717224d8b83bc0ab9ea2dc08f47b33db1e20cfe7
geo <36279334+1lewisgeo@users.noreply.github.com>
2018-02-17 14:41:23 -0500

    Streamlined Robot class

--- src/main/java/frc/team4902/robot/Robot.java
+++ src/main/java/frc/team4902/robot/Robot.java
@@ -1,18 +1,25 @@
 package frc.team4902.robot;
 
 import edu.wpi.first.wpilibj.ADXRS450_Gyro;
-import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.TimedRobot;
-import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.command.Scheduler;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import frc.team4902.robot.subsystems.DriveSystem;
-import frc.team4902.robot.subsystems.ElevatorPosition;
 import frc.team4902.robot.subsystems.ElevatorSystem;
 import frc.team4902.robot.subsystems.GripSystem;
 
 public class Robot extends TimedRobot {
 
 	public static ADXRS450_Gyro gyro = new ADXRS450_Gyro();
+	
+	public static SendableChooser<Integer> inputModeChooser = new SendableChooser<>();
+	
+	static {
+		inputModeChooser.addDefault("First", 1);
+		inputModeChooser.addObject("Second", 2);
+		SmartDashboard.putData(inputModeChooser);
+	}
 
 	@Override
 	public void robotInit() {
